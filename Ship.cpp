e6086c357ddbe445dba24c0e67a2c6119a3763b1
adase <adasek@mathesio.cz>
2022-12-02 13:12:03 +0100

    ship

---
+++ Ship.cpp
@@ -0,0 +1,5 @@
+//
+// Created by adase on 2. 12. 2022.
+//
+
+#include "Ship.h"
---
+++ Ship.h
@@ -0,0 +1,17 @@
+//
+// Created by adase on 2. 12. 2022.
+//
+
+#ifndef IMS_SHIP_H
+#define IMS_SHIP_H
+
+
+class Starting_Ship {
+public:
+    int Capacity;
+
+    const int AVARAGE_AVAILABLE_TIME_IN_PORT =
+};
+
+
+#endif //IMS_SHIP_H
